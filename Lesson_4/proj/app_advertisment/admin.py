183b726a5ba726e38fc60d82162e1d824a3c9c0c
Vladislav <mr.ender555@gmail.com>
2023-08-25 22:15:11 +0500

    Homework

--- Lesson_4/proj/app_advertisment/admin.py
+++ Lesson_4/proj/app_advertisment/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
 from .models import Advertisement
+from django.db.models.query import QuerySet
+from django.utils.safestring import mark_safe
 
+# Register your models here.
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'text', 'author', 'auction', 'price']
+    list_display = ['id', 'title', 'text', 'author', 'price', 'auction', 'created_date', 'update_date', 'get_image'] # 
     list_filter = ['author', 'title']
-    actions = ['delete_description', 'make_auction_as_true']
+    actions = ['delete_description', 'make_auction_as_true', 'make_auction_as_false']
+    readonly_fields = ["get_image"]
+    # fieldsets = ( # при добавлении записей указываем кастомизацию
+    #     (   # блок 1
+    #         'Общее', # название блока
+    #         {
+    #             "fields":('title', 'text', 'author'),
+    #         }
+    #     ),
+    #     (   # блок 2
+    #         'Финансы', # название блока
+    #         {
+    #             "fields":('price', 'auction'),
+    #             "classes":['collapse'] # для скрытия
+    #         }
+    #     ) 
+    # )
+    
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+    
+    get_image.short_description = "Изображение"
     
     
     @admin.action(description="Удалить описание выбранных объектов")
@@ -13,8 +37,16 @@ class AdvertisementAdmin(admin.ModelAdmin):
         queryset.update(text='')
         
     @admin.action(description="Включить возможность торга")
-    def make_auction_as_true(self, request, queryset):
+    def make_auction_as_true(self, request, queryset:QuerySet):
+        print(queryset, type(queryset))
         queryset.update(auction=True)
+
+    @admin.action(description="Отключить возможность торга")
+    def make_auction_as_false(self, request, queryset:QuerySet):
+        print(queryset, type(queryset))
+        queryset.update(auction=False)
+
     
+    
+
 admin.site.register(Advertisement, AdvertisementAdmin)
-# Register your models here.
--- Lesson_4/proj/app_advertisment/models.py
+++ Lesson_4/proj/app_advertisment/models.py
@@ -1,19 +1,45 @@
 from django.db import models
 from django.contrib import admin
 from django.utils import timezone, html
-
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
 # Create your models here.
+User = get_user_model()
 
-class Advertisement(models.Model): # это класс-модель
-    # он реализует таблицу Advertisement
+class Advertisement(models.Model): # это класс-модель 
+    # он реализует таблицу Advertisment
     title = models.CharField("заголовок", max_length=128)
     text = models.TextField("описание")
-    author = models.CharField("автор", max_length=64)
     price = models.FloatField("цена")
     auction = models.BooleanField("торг", default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField("изображение", upload_to='media/', default='static/img/adv.png') 
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "advertisements"
        
-    def __str__(self):
-        return f"title: {self.title}, text: {self.text}"
+    def __str__(self) -> str:
+        return f"Advertisements(id = {self.id}, title = {self.title}, price = {self.price})"
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            create_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color:green; font-weight: bold;">Сегодня в {}</span>', create_time
+            )
+        return self.created_at.strftime('%d.%m.%y at %H:%M:%S')
         
+
+    @admin.display(description="Дата обновления")
+    def update_date(self):
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color:purple; font-weight: bold;">Сегодня в {}</span>', update_time
+            )
+        return self.update_at.strftime('%d.%m.%y at %H:%M:%S')
+    
